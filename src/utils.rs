e7e705a7ff132d163a12dea69bf592501376a7f6
Mike Zeller <mike@mikezeller.net>
2017-02-19 15:26:01 -0500

    Change to_owned to clone

--- src/utils.rs
+++ src/utils.rs
@@ -4,13 +4,13 @@ use serde_json::Value;
 use error::{Result, Error};
 
 pub fn hue_result(v: Value) -> Result<Value> {
-	if let Some(val) = v.get(0).and_then(|v| v.get("success")) {
-        return Ok(val.to_owned());
-	}
-	if let Some(err) = v.get(0).and_then(|v| v.get("error")) {
-		let desc = err["description"].as_str().unwrap_or("Hue error")
+    if let Some(val) = v.get(0).and_then(|v| v.get("success")) {
+        return Ok(val.clone());
+    }
+    if let Some(err) = v.get(0).and_then(|v| v.get("error")) {
+        let desc = err["description"].as_str().unwrap_or("Hue error")
             .to_owned();
-		return Err(Error::Hue(desc));
-	}
+        return Err(Error::Hue(desc));
+    }
     Err(Error::Other("Hue Bridge sent back bad data"))
 }
